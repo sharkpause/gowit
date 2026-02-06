b3b7d90f4809f1a599c71a32d61befea9a3eb7a1
sharkpause <carlmabfredishere@gmail.com>
2026-02-06 20:43:20 +0700

    feat: made db connectoin

---
+++ src/server/db/db.go
@@ -0,0 +1,24 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var DB *sql.DB
+
+func Connect() (*sql.DB, error) {
+	dsn := fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s?parseTime=true",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+	)
+
+	return sql.Open("mysql", dsn)
+}
--- src/server/main.go
+++ src/server/main.go
@@ -2,18 +2,36 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/gin-gonic/gin"
-	"github.com/sharkpause/gowit/config"
+	"github.com/joho/godotenv"
+	"github.com/sharkpause/gowit/db"
 	"github.com/sharkpause/gowit/routes"
 )
 
 func main() {
 	fmt.Println("Starting server, please wait...")
 
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	router := gin.Default()
+
+	db, err := db.Connect()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected successfully to database")
 
 	routes.SetupAPIRoutes(router)
 
-	router.Run(config.LISTENING_PORT)
+	router.Run(os.Getenv("SERVER_PORT"))
 }
