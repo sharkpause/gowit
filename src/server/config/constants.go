6cb19fefea60c7e092c2b35d4a812ae66d4e1d37
sharkpause <carlmabfredishere@gmail.com>
2026-02-06 20:43:37 +0700

    docs: added comments in constants to address empty file

--- src/server/config/constants.go
+++ src/server/config/constants.go
@@ -1,5 +1,10 @@
 package config
 
 const (
-	LISTENING_PORT = ":8080"
+	// LISTENING_PORT = ":8080"
 )
+
+// TODO: Figure out something to do with this
+// This used to hold configs like listening port, http status codes, but now it's empty
+// Either delete it or keep it for future just in case we need it
+// For now ig keep it
