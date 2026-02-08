e30ff2f1e2e25e6ebe679f31c27c7cb35547b99f
sharkpause <carlmabfredishere@gmail.com>
2026-02-08 22:34:35 +0700

    docs: add explanation to the usage of pointers

--- src/server/models/movie.go
+++ src/server/models/movie.go
@@ -1,8 +1,13 @@
 package models
 
+// pointers are needed for fields that are nullable
+// nil, null
+// &"foo", a value
+// &"", empty
+
 type Movie struct {
-	ID				int		`json:"id"`
-	Title 			string	`json:"title"`
+	ID				int			`json:"id"`
+	Title 			string		`json:"title"`
 	Description		*string 	`json:"description"`
 	ReleaseYear 	*int		`json:"release_year"`
 }
