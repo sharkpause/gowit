a7e005d4dbda4228eaac47b86b7a9d745ee6869a
FredlyMarvander <fredly210307@gmail.com>
2026-02-11 19:35:38 +0700

    Add Register and Login Page

--- src/client/gowit/src/App.tsx
+++ src/client/gowit/src/App.tsx
@@ -2,12 +2,16 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage";
 import Layout from "./layout/layout";
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/login" element={<LoginPage />} />
           <Route path="/" element={<Layout />}>
             <Route path="" element={<HomePage />} />
           </Route>
