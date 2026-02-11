aacb63fb731838b294bd182ed2e28fa7c6532931
sharkpause <carlmabfredishere@gmail.com>
2026-02-11 18:12:10 +0700

    feat: made register handler

--- src/server/handlers/users.go
+++ src/server/handlers/users.go
@@ -3,14 +3,16 @@ package handlers
 import (
 	"database/sql"
 	"net/http"
+	"net/mail"
 
 	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type registerRequest struct {
-	Name		string `json:name`
-	Email		string `json:email`
-	Password	string `json:password`
+	Name		string `json:"name"`
+	Email		string `json:"email"`
+	Password	string `json:"password"`
 }
 
 func RegisterUser(database *sql.DB) func(*gin.Context) {
@@ -23,6 +25,95 @@ func RegisterUser(database *sql.DB) func(*gin.Context) {
 			return
 		}
 
-		context.JSON(http.StatusOK, gin.H{"message": "OK"})
+		if request.Name == "" {
+			context.JSON(http.StatusBadRequest, gin.H{
+				"error": "name is required",
+			})
+			return
+		}
+		if request.Email == "" {
+			context.JSON(http.StatusBadRequest, gin.H{
+				"error": "email is required",
+			})
+			return
+		}
+		if request.Password == "" {
+			context.JSON(http.StatusBadRequest, gin.H{
+				"error": "password is required",
+			})
+			return
+		}
+
+		if len(request.Name) > 255 {
+			context.JSON(http.StatusBadRequest, gin.H{
+				"error": "name can not be over 255 characters long",
+			})
+			return
+		}
+		if len(request.Email) > 255 {
+			context.JSON(http.StatusBadRequest, gin.H{
+				"error": "name can not be over 255 characters long",
+			})
+			return
+		}
+
+		// TODO: Add password requirements
+
+		_, err := mail.ParseAddress(request.Email)
+		if err != nil {
+			context.JSON(http.StatusBadRequest, gin.H{
+				"error": "invalid email format",
+			})
+			return
+		}
+
+		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(request.Password), bcrypt.DefaultCost)
+		if err != nil {
+			context.JSON(http.StatusInternalServerError, gin.H{
+				"error": "failed to hash password",
+			})
+			return
+		}
+
+		var existingID uint64
+		err = database.QueryRow(
+			`SELECT id FROM users where email = ?`,
+			request.Email,
+		).Scan(&existingID)
+
+		if err != sql.ErrNoRows {
+			context.JSON(http.StatusBadRequest, gin.H{
+				"error": "email already in use",
+			})
+			return
+		}
+
+		result, err := database.Exec(
+			`INSERT INTO users (name, email, password_hash)
+			VALUES (
+				?, ?, ?
+			)`,
+			request.Name, request.Email, hashedPassword,
+		)
+
+		if err != nil {
+			context.JSON(http.StatusInternalServerError, gin.H{
+				"error": "internal db error",
+			})
+			return
+		}
+
+		userID, err := result.LastInsertId()
+		if err != nil {
+			context.JSON(http.StatusInternalServerError, gin.H{
+				"error": "could not retrieve user ID",
+			})
+			return
+		}
+
+		context.JSON(http.StatusOK, gin.H{
+			"message": "user registered successfully",
+			"user_id": userID,
+		})
 	}
 }
