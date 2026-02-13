06266cddff41dfba6708b108bf36b8aeff31d7f7
sharkpause <carlmabfredishere@gmail.com>
2026-02-13 23:12:55 +0700

    feat: add error handling when JWT_SECRET is empty or not set

--- src/server/auth/jwt.go
+++ src/server/auth/jwt.go
@@ -1,6 +1,7 @@
 package auth
 
 import (
+	"fmt"
 	"os"
 	"time"
 
@@ -13,11 +14,18 @@ func GenerateJWT(userID uint64) (string, error) {
 				"user_id": userID,
 				"exp": time.Now().Add(time.Hour * 24 * 30).Unix(),
 			}
+
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	signedToken, err := token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+	
+	jwtSecret := os.Getenv("JWT_SECRET")
+	if jwtSecret == "" {
+		return "", fmt.Errorf("JWT_SECRET is not set")
+	}
+
+	signedToken, err := token.SignedString([]byte(jwtSecret))
 	if err != nil {
 		return "", err
 	}
+
 	return signedToken, err
-	
 }
