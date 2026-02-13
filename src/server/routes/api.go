f4595fd67305c89178a43c04e198d45ed2894ac7
sharkpause <carlmabfredishere@gmail.com>
2026-02-13 22:26:52 +0700

    fix: commented out auth.Test that was undefined

--- src/server/routes/api.go
+++ src/server/routes/api.go
@@ -20,7 +20,11 @@ func SetupAPIRoutes(router *gin.Engine, database *sql.DB) {
 
 		api.POST("/register", handlers.RegisterUser(database))
 		api.POST("/login", handlers.LoginUser(database))
-		protected:= router.Group("/")
+
+		// TODO: Later change so that protected APIs are still accessed through {URL}/api and not {URL}/
+		// to keep API consistency
+
+		protected := router.Group("/")
 		protected.Use(auth.Middleware())
 		
 		{
@@ -28,6 +32,8 @@ func SetupAPIRoutes(router *gin.Engine, database *sql.DB) {
 		}
 		
 		api.Use(auth.Middleware())
-		api.GET("/test", auth.Test)
+
+		// auth.Test undefined, will ask Carvin for details later
+		// api.GET("/test", auth.Test)
 	}
 }
