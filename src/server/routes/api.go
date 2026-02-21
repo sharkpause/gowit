90bc743583f47b7ef277028e25b493be51cae081
sharkpause <carlmabfredishere@gmail.com>
2026-02-21 13:57:36 +0700

    feat: add 8080 as an allowed port in cors

--- src/server/routes/api.go
+++ src/server/routes/api.go
@@ -17,6 +17,7 @@ func SetupAPIRoutes(router *gin.Engine, database *sql.DB) {
 		AllowOrigins: []string{
 			"http://localhost:3000",
 			"http://localhost:5173",
+			"http://localhost:8080",
 			// "https://domain-frontend-kamu.com",
 		},
 		AllowMethods: []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
