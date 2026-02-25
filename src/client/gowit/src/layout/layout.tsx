911e2bcef3ee55b970452e8e623548fff6311005
FredlyMarvander <fredly210307@gmail.com>
2026-02-25 21:09:16 +0700

    On progress on FE middleware

--- src/client/gowit/src/layout/layout.tsx
+++ src/client/gowit/src/layout/layout.tsx
@@ -1,9 +1,22 @@
 import { Outlet } from "react-router";
 import Navbar from "../components/Navbar";
 import { serverApi } from "../api";
+import { useEffect } from "react";
 
-export default async function Layout() {
-  const response = await serverApi.get("/api/me");
+export default function Layout() {
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await serverApi.get("/api/me");
+        console.log(response);
+      } catch (error) {
+        console.log("Error at Layout:", error);
+      }
+    };
+
+    fetchUser();
+  }, []);
+
   return (
     <>
       <Navbar />
