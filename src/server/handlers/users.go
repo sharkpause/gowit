4c1c8d5d6ba2cb676cea823d65a30d974f0303f8
hoowwshii <carvinxwinarli@gmail.com>
2026-02-28 20:23:49 +0700

    adding userdetail -> userprofile. idk

--- src/server/handlers/users.go
+++ src/server/handlers/users.go
@@ -4,10 +4,11 @@ import (
 	"database/sql"
 	"net/http"
 	"net/mail"
+	"time"
 
 	"github.com/gin-gonic/gin"
-	"golang.org/x/crypto/bcrypt"
 	"github.com/sharkpause/gowit/auth"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type registerRequest struct {
@@ -187,4 +188,44 @@ func LogoutUser(context *gin.Context){
 	context.JSON(200, gin.H{
 		"message":"Logged Out",
 	})
+}
+
+func UserDetail(database *sql.DB) gin.HandlerFunc{
+	return func(context *gin.Context) {
+		userID,exists := context.Get("user_id")
+			if !exists {
+				context.JSON(401, gin.H{"message": "Unauthorized",})
+				return
+			}
+		userID=userID.(uint64)
+		
+		var name,email,profile_picture_url string
+		var created_at time.Time
+		query := "SELECT name, email, profile_picture_url, created_at FROM users WHERE id = ?;"
+		
+		err:=database.QueryRow(query,userID).Scan(
+			&name,
+			&email,
+			&profile_picture_url,
+			&created_at,
+		)
+		
+		if err != nil{
+			context.JSON(500, gin.H{"message": "Internal Database Error"})
+			return
+		}
+		context.JSON(200, gin.H{
+			"name": name,
+			"email": email,
+			"profile": profile_picture_url,
+			"created": created_at.Format(time.RFC3339), // chatgpt told me its uhhh some 2026-02-14T23:49:22Z, didnt understand shit, but okay. let the frontend do their magic
+			// 			
+			//     "created": "2026-02-14T23:49:22Z",
+			//     "email": "john@example.com",
+			//     "name": "John Doe",
+			//     "profile": "nulnot"
+			//  	also returning like this, map. im too rude you know what he said to me, he was like 'you are so rude' and i was like boy does it look like i could care i couldnt even care less rude
+		})
+	}
+	
 }
