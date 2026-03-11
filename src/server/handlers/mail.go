932331eb407b42d0fac8893afb470e1027d98e99
sharkpause <carlmabfredishere@gmail.com>
2026-03-11 20:42:27 +0700

    refactor: reformatting

--- src/server/handlers/mail.go
+++ src/server/handlers/mail.go
@@ -16,61 +16,76 @@ type ContactRequest struct {
 }
 func Sendmail() gin.HandlerFunc{
 	return func(context *gin.Context){
-	var req ContactRequest
-	if err:= context.ShouldBindJSON(&req); err!=nil{
-		context.JSON(400,gin.H{"Error": err.Error()})
-		return
-	}
-	mail := gomail.NewMessage()
-	system:= struct {
-		SystemEmail string 
-		EmailSecret string
-		EmailServer string
-		EmailPort int
-		BccMailingList []string
-	}{
-		SystemEmail: os.Getenv("ADMIN_EMAIL"), //<Email>
-		EmailSecret: os.Getenv("ADMIN_SECRET"), //<Google app password to the corresponding account>
-		EmailServer: os.Getenv("EMAIL_SERVER"), //<smtp.gmail.com> if using google, yea 
-		EmailPort: func()int{
-			p,_:= strconv.Atoi(os.Getenv("EMAIL_PORT")) //587
-			return p
-		}(),
-		BccMailingList: strings.Split(os.Getenv("MAILING_LIST"),","), //<Bcc> can be null, depends
-	}
-	mail.SetHeader("From", system.SystemEmail)
-	mail.SetHeader("To", req.Email)
-	mail.SetHeader("Cc", system.SystemEmail)
-	if len(system.BccMailingList) > 0{
-		mail.SetHeader("Bcc", system.BccMailingList...)
-	}
-	mail.SetHeader("Reply-To", req.Email)
-	body:= fmt.Sprintf(`
-Dear %s,
+		var req ContactRequest
+		
+		if err:= context.ShouldBindJSON(&req); err!=nil{
+			context.JSON(400,gin.H{"Error": err.Error()})
+			return
+		}
 
-Thank you for contacting Gowit.
+		mail := gomail.NewMessage()
+		
+		system := struct {
+			SystemEmail string 
+			EmailSecret string
+			EmailServer string
+			EmailPort int
+			BccMailingList []string
+		}{
+			SystemEmail: os.Getenv("ADMIN_EMAIL"), //<Email>
+			
+			EmailSecret: os.Getenv("ADMIN_SECRET"), //<Google app password to the corresponding account>
+			
+			EmailServer: os.Getenv("EMAIL_SERVER"), //<smtp.gmail.com> if using google, yea 
+			
+			EmailPort: func()int{
+				p,_:= strconv.Atoi(os.Getenv("EMAIL_PORT")) //587
+				return p
+			}(),
+			
+			BccMailingList: strings.Split(os.Getenv("MAILING_LIST"),","), //<Bcc> can be null, depends
+		}
 
-We have received your message and our support team will review your inquiry. If further assistance is required, we will get back to you as soon as possible.
-Your message:
---------------------------------
-%s
---------------------------------
-Please note that response times may vary depending on the volume of requests.
+		mail.SetHeader("From", strings.TrimSpace(system.SystemEmail))
+		mail.SetHeader("To", strings.TrimSpace(req.Email))
+		mail.SetHeader("Cc", system.SystemEmail)
+		
+		if len(system.BccMailingList) > 0{
+			mail.SetHeader("Bcc", system.BccMailingList...)
+		}
+		mail.SetHeader("Reply-To", req.Email)
+		
+		body := fmt.Sprintf(`
+			Dear %s,
 
-We appreciate your patience and thank you for reaching out.
+			Thank you for contacting Gowit.
 
-Best regards,  
-Gowit Support Team`, 
-req.Name,req.Question)
-	mail.SetBody("text/plain", body)
-	dial := gomail.NewDialer(system.EmailServer,system.EmailPort,system.SystemEmail,system.EmailSecret)
-	e:=dial.DialAndSend(mail)
-	if e!=nil{
-		context.JSON(500, gin.H{"Error":"Failed to send email"})
-		return
+			We have received your message and our support team will review your inquiry. If further assistance is required, we will get back to you as soon as possible.
+			Your message:
+			--------------------------------
+			%s
+			--------------------------------
+			Please note that response times may vary depending on the volume of requests.
+
+			We appreciate your patience and thank you for reaching out.
+
+			Best regards,  
+			Gowit Support Team`, 
+			
+			req.Name, req.Question)
+		
+		mail.SetBody("text/plain", body)
+		dial := gomail.NewDialer(system.EmailServer,system.EmailPort,system.SystemEmail,system.EmailSecret)
+
+		e:=dial.DialAndSend(mail)
+				
+		if e!=nil{
+			context.JSON(500, gin.H{"Error":"Failed to send email"})
+			fmt.Println(e)
+			return
+		}
+		context.JSON(200, gin.H{
+			"message": "Message sent",
+		})
 	}
-	context.JSON(200, gin.H{
-		"message": "Message sent",
-	})
-}
 }
