f185e4e223ff50a145e52c71c00c375b8372e5d9
hoowwshii <carvinxwinarli@gmail.com>
2026-03-14 19:20:22 +0700

    flip that that that that that that that dwijipeo dwijipeo jeonbu flipthat

--- src/server/handlers/comments.go
+++ src/server/handlers/comments.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"database/sql"
+	"fmt"
 	"net/http"
 	"strconv"
 
@@ -58,4 +59,96 @@ func CreateComment(database *sql.DB) gin.HandlerFunc {
 			"comment_id":id,
 		})
 	}
+}
+
+func Likey(database *sql.DB) gin.HandlerFunc{ //https://music.apple.com/id/album/likey/1555405141?i=1555405145
+	return func(context *gin.Context){
+		userID,exists:=context.Get("user_id")
+		if !exists {
+			context.JSON(401, gin.H{"message": "Unauthorized",})
+			return
+		}
+
+		var vote models.CommentVote
+		if err:= context.ShouldBindJSON(&vote); err != nil{
+			context.JSON(http.StatusBadRequest, gin.H{
+				"message": "Invalid request body",
+			})
+			return
+		}
+
+		if vote.Score != -1 && vote.Score != 1{
+			context.JSON(http.StatusBadRequest, gin.H{
+				"message": "invalid score: must be the water -1 or 1",
+			})
+			return
+		}
+
+		vote.UserID = userID.(uint64)
+		_,err := database.Exec(`
+		INSERT INTO comments_vote (comment_id,user_id,score)
+		VALUES (
+			?,?,?
+		)
+		ON DUPLICATE KEY UPDATE score = VALUES(score)
+		`,
+		vote.CommentID,vote.UserID,vote.Score)
+		
+		if err != nil {
+			context.JSON(http.StatusInternalServerError,gin.H{
+				"message": "Database update failed",
+			})
+			return
+		}
+		context.JSON(200, gin.H{
+			"message": "Added successfullly",
+		})
+	}
+}
+
+
+func GetCommentByFilmID(database *sql.DB) gin.HandlerFunc{ // this will only list parent and not child
+	return func(context *gin.Context) {
+		film_id,err := strconv.ParseUint(context.Param("id"),10,64)
+		if err != nil{
+			context.JSON(http.StatusBadRequest, gin.H{
+				"message":"Invalid film id",
+			})
+			return
+		}
+
+		rows,err := database.Query( // 																		reply count
+		`SELECT c.id, c.film_id, c.user_id, u.name, c.parent_id, c.content, c.created_at, (SELECT COUNT(*) FROM comments WHERE parent_id = c.id) AS reply_count, COALESCE(SUM(cv.score),0) AS vote_count
+		FROM comments c 
+		JOIN users u ON u.id = c.user_id
+		LEFT JOIN comments_vote cv ON cv.comment_id = c.id
+		WHERE film_id = ? AND parent_id IS NULL 
+		GROUP BY c.id, c.film_id, c.user_id, u.name, c.parent_id, c.content, c.created_at
+		ORDER BY created_at ASC`, film_id)
+
+		if err != nil {
+			fmt.Println(err)
+			context.JSON(http.StatusInternalServerError, gin.H{
+				"message":"Internal server error",
+			})
+			return 
+		}
+
+		defer rows.Close()
+		var comments []models.Comment
+		for rows.Next() {
+			var c models.Comment
+			err := rows.Scan(&c.ID,&c.FilmID,&c.UserID,&c.UserName,&c.ParentID,&c.Content,&c.CreatedAt,&c.ReplyCount,&c.VoteCount)
+			if err != nil{
+				return 
+			}
+			comments = append(comments,c)
+		}
+
+		if comments == nil{
+			comments = []models.Comment{}
+		}
+		context.JSON(http.StatusOK, comments)
+	}
+
 }
