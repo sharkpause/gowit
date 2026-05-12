6ea15f89a2df69ca2b570e1d1ca7dd930ceaad7f
sharkpause <carlmabfredishere@gmail.com>
2026-05-12 18:39:55 +0700

    fix: move watchlist sharing to unprotected

--- src/server/routes/api.go
+++ src/server/routes/api.go
@@ -61,15 +61,15 @@ func SetupAPIRoutes(router *gin.Engine, database *sql.DB, restrictedWords map[st
 
 		api.GET("/films/:id/summary", handlers.GetFilmSummary(database, geminiClient))
 
+		api.GET("/user/favorites/share", handlers.GenerateFavoriteShareCode(database))
+		api.GET("/user/favorites/:code", handlers.GetSharedWatchlist(database))
+
 		// TODO: Later change so that protected APIs are still accessed through {URL}/api and not {URL}/
 		// to keep API consistency
 
 		protected := api.Group("/") // 我tmd有点儿醉，修改了，请查看是否有没有问题 pllease check, saya rase sudah okehyyyhhhhhhhhh。 i 婷 it was router.group('/'); idk if its wrong or not, but it works with /api/test…… no idea. but the correct way of doing it maybe looks like deez
 		protected.Use(auth.Middleware())
 		{
-			protected.GET("/user/favorites/share", handlers.GenerateFavoriteShareCode(database))
-			protected.GET("/user/favorites/:code", handlers.GetSharedWatchlist(database))
-
 			protected.GET("/favorites", handlers.GetFavorites(database))
 			// protected.GET("/test", auth.Test) //auffth.Test just ignore, will delete later, forgot to put into .gitignore. mb.
 			protected.POST("/favorites", handlers.AddFilmToFavorite(database)) // dear @gilbert-sunbaenim, 我不知道要放什么route，只好先这样咯，
